7fe4f2adcd9e42956d3114561adf69d73fcf7b1c
nikitin_sas <nikitinsasha323@gmail.com>
2025-05-06 17:09:18 +0300

    update project-config

---
+++ Kafka_service/internal/config/config.go
@@ -0,0 +1,50 @@
+package configs
+
+import (
+	"log"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Kafka  KafkaConfig  `mapstructure:"kafka"`
+	Logger LoggerConfig `mapstructure:"logger"`
+}
+type KafkaConfig struct {
+	BootstrapServers string        `mapstructure:"bootstrap_servers"`
+	RetryBackoffMs   int           `mapstructure:"retry_backoff_ms"`
+	GroupId          string        `mapstructure:"group_id"`
+	Topics           KafkaTopics   `mapstructure:"topics"`
+	AutoCommit       bool          `mapstructure:"enable_auto_commit"`
+	SessionTimeout   time.Duration `mapstructure:"session_timeout"`
+	HearbeatInterval time.Duration `mapstructure:"heartbeat_interval"`
+}
+type KafkaTopics struct {
+	InfoLog  string `mapstructure:"info_log"`
+	ErrorLog string `mapstructure:"error_log"`
+	WarnLog  string `mapstructure:"warn_log"`
+}
+type LoggerConfig struct{}
+
+func LoadConfig() Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath("internal/configs")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Printf("[ERROR] [Kafka-Service] Config file not found; using defaults or environment variables")
+		} else {
+			log.Fatalf("[ERROR] [Kafka-Service] Error reading config file: %s", err)
+		}
+	}
+	var config Config
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Fatalf("[ERROR] [Kafka-Service] Unable to decode into struct, %v", err)
+	}
+	log.Println("[INFO] [Kafka-Service] Successful Load Config")
+	return config
+}
