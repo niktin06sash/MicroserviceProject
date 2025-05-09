8d4d0b2c8af7367fe0e65ebb3d8085b32b6e9af1
nikitin_sas <nikitinsasha323@gmail.com>
2025-05-09 16:17:57 +0300

    implement kafka in service

--- SessionManagement_service/internal/service/service.go
+++ SessionManagement_service/internal/service/service.go
@@ -2,21 +2,18 @@ package service
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/google/uuid"
-	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/erro"
-	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/logger"
+	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/kafka"
 	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/repository"
 	pb "github.com/niktin06sash/MicroserviceProject/SessionManagement_service/proto"
-	"go.uber.org/zap"
 	"google.golang.org/grpc/metadata"
 )
 
 type SessionAPI struct {
 	pb.UnimplementedSessionServiceServer
 	sessionService SessionAuthentication
-	logger         *logger.SessionLogger
+	kafkaProducer  kafka.KafkaProducerService
 }
 type SessionAuthentication interface {
 	CreateSession(ctx context.Context, req *pb.CreateSessionRequest) (*pb.CreateSessionResponse, error)
@@ -24,62 +21,50 @@ type SessionAuthentication interface {
 	DeleteSession(ctx context.Context, req *pb.DeleteSessionRequest) (*pb.DeleteSessionResponse, error)
 }
 
-func getRequestIdFromMetadata(ctx context.Context, log *logger.SessionLogger, place string) string {
+func NewSessionAPI(repos *repository.Repository, kafka kafka.KafkaProducerService) *SessionAPI {
+	return &SessionAPI{
+		sessionService: NewSessionService(repos.RedisSessionRepos, kafka),
+		kafkaProducer:  kafka,
+	}
+}
+func (s *SessionAPI) CreateSession(ctx context.Context, req *pb.CreateSessionRequest) (*pb.CreateSessionResponse, error) {
+	var place = "CreateSession"
+	traceID := s.getTraceIdFromMetadata(ctx, place)
+	ctx = context.WithValue(ctx, "traceID", traceID)
+	resp, err := s.sessionService.CreateSession(ctx, req)
+	s.kafkaProducer.NewSessionLog(kafka.LogLevelInfo, place, traceID, "Succesfull send response to client")
+	return resp, err
+}
+
+func (s *SessionAPI) ValidateSession(ctx context.Context, req *pb.ValidateSessionRequest) (*pb.ValidateSessionResponse, error) {
+	var place = "ValidateSession"
+	traceID := s.getTraceIdFromMetadata(ctx, place)
+	ctx = context.WithValue(ctx, "traceID", traceID)
+	resp, err := s.sessionService.ValidateSession(ctx, req)
+	s.kafkaProducer.NewSessionLog(kafka.LogLevelInfo, place, traceID, "Succesfull send response to client")
+	return resp, err
+}
+
+func (s *SessionAPI) DeleteSession(ctx context.Context, req *pb.DeleteSessionRequest) (*pb.DeleteSessionResponse, error) {
+	var place = "DeleteSession"
+	traceID := s.getTraceIdFromMetadata(ctx, place)
+	ctx = context.WithValue(ctx, "traceID", traceID)
+	resp, err := s.sessionService.DeleteSession(ctx, req)
+	s.kafkaProducer.NewSessionLog(kafka.LogLevelInfo, place, traceID, "Succesfull send response to client")
+	return resp, err
+}
+func (s *SessionAPI) getTraceIdFromMetadata(ctx context.Context, place string) string {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		log.Warn(fmt.Sprintf("[%s] Metadata not found in context", place), zap.Error(erro.ErrorMissingMetadata))
+		s.kafkaProducer.NewSessionLog(kafka.LogLevelWarn, place, "", "Metadata not found in context")
 		newreq := uuid.New()
 		return newreq.String()
 	}
 	traceIDs := md.Get("traceID")
 	if len(traceIDs) == 0 || traceIDs[0] == "" {
-		log.Warn(fmt.Sprintf("[%s] Trace ID not found in context", place), zap.Error(erro.ErrorRequiredTraceID))
+		s.kafkaProducer.NewSessionLog(kafka.LogLevelWarn, place, "", "Trace ID not found in context")
 		newtrace := uuid.New()
 		return newtrace.String()
 	}
 	return traceIDs[0]
-}
-func NewSessionAPI(repos *repository.Repository, log *logger.SessionLogger) *SessionAPI {
-	return &SessionAPI{
-		sessionService: NewSessionService(repos.RedisSessionRepos, log),
-		logger:         log,
-	}
-}
-func (s *SessionAPI) CreateSession(ctx context.Context, req *pb.CreateSessionRequest) (*pb.CreateSessionResponse, error) {
-	traceID := getRequestIdFromMetadata(ctx, s.logger, "CreateSession")
-	ctx = context.WithValue(ctx, "traceID", traceID)
-	resp, err := s.sessionService.CreateSession(ctx, req)
-	s.logger.Info("CreateSession: Succesfull send response to client",
-		zap.String("traceID", traceID),
-	)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
-}
-
-func (s *SessionAPI) ValidateSession(ctx context.Context, req *pb.ValidateSessionRequest) (*pb.ValidateSessionResponse, error) {
-	traceID := getRequestIdFromMetadata(ctx, s.logger, "ValidateSession")
-	ctx = context.WithValue(ctx, "traceID", traceID)
-	resp, err := s.sessionService.ValidateSession(ctx, req)
-	s.logger.Info("ValidateSession: Succesfull send response to client",
-		zap.String("traceID", traceID),
-	)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
-}
-
-func (s *SessionAPI) DeleteSession(ctx context.Context, req *pb.DeleteSessionRequest) (*pb.DeleteSessionResponse, error) {
-	traceID := getRequestIdFromMetadata(ctx, s.logger, "DeleteSession")
-	ctx = context.WithValue(ctx, "traceID", traceID)
-	resp, err := s.sessionService.DeleteSession(ctx, req)
-	s.logger.Info("DeleteSession: Succesfull send response to client",
-		zap.String("traceID", traceID),
-	)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
 }
--- SessionManagement_service/internal/service/service_session.go
+++ SessionManagement_service/internal/service/service_session.go
@@ -5,12 +5,10 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/erro"
-	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/logger"
+	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/kafka"
 	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/model"
 	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/repository"
 	pb "github.com/niktin06sash/MicroserviceProject/SessionManagement_service/proto"
-	"go.uber.org/zap"
 
 	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
@@ -18,43 +16,37 @@ import (
 )
 
 type SessionService struct {
-	repo   repository.RedisSessionRepos
-	logger *logger.SessionLogger
+	repo          repository.RedisSessionRepos
+	kafkaProducer kafka.KafkaProducerService
 }
 
-func NewSessionService(repo repository.RedisSessionRepos, log *logger.SessionLogger) *SessionService {
-	return &SessionService{repo: repo, logger: log}
+func NewSessionService(repo repository.RedisSessionRepos, kafka kafka.KafkaProducerService) *SessionService {
+	return &SessionService{repo: repo, kafkaProducer: kafka}
 }
-func validateContext(ctx context.Context, logger *logger.SessionLogger, place string) (string, error) {
+func (s *SessionService) validateContext(ctx context.Context, place string) (string, error) {
 	traceID := ctx.Value("traceID").(string)
 	select {
 	case <-ctx.Done():
-		logger.Error(fmt.Sprintf("[%s] Context time-out", place),
-			zap.String("traceID", traceID),
-			zap.Error(ctx.Err()),
-		)
+		fmterr := fmt.Sprintf("Context error: %v", ctx.Err())
+		s.kafkaProducer.NewSessionLog(kafka.LogLevelError, place, traceID, fmterr)
 		return "", status.Errorf(codes.Internal, "Request timed out")
 	default:
 		return traceID, nil
 	}
 }
 func (s *SessionService) CreateSession(ctx context.Context, req *pb.CreateSessionRequest) (*pb.CreateSessionResponse, error) {
-	traceID, err := validateContext(ctx, s.logger, "CreateSession")
+	var place = "CreateSession"
+	traceID, err := s.validateContext(ctx, place)
 	if err != nil {
 		return nil, err
 	}
 	if req.UserID == "" {
-		s.logger.Error("CreateSession: Required userID",
-			zap.String("traceID", traceID),
-			zap.Error(erro.ErrorRequiredUserId),
-		)
+		s.kafkaProducer.NewSessionLog(kafka.LogLevelError, place, traceID, "Required userID")
 		return nil, status.Errorf(codes.InvalidArgument, "UserID is required")
 	}
 	if _, err := uuid.Parse(req.UserID); err != nil {
-		s.logger.Error("CreateSession: Error UUID-Parse userID",
-			zap.String("traceID", traceID),
-			zap.Error(err),
-		)
+		fmterr := fmt.Sprintf("UUID-Parse userID error: %v", err)
+		s.kafkaProducer.NewSessionLog(kafka.LogLevelError, place, traceID, fmterr)
 		return nil, status.Errorf(codes.InvalidArgument, "Invalid userID format: %v", err)
 	}
 	sessionID := uuid.New()
@@ -75,22 +67,17 @@ func (s *SessionService) CreateSession(ctx context.Context, req *pb.CreateSessio
 	}, nil
 }
 func (s *SessionService) ValidateSession(ctx context.Context, req *pb.ValidateSessionRequest) (*pb.ValidateSessionResponse, error) {
-	traceID, err := validateContext(ctx, s.logger, "ValidateSession")
+	var place = "ValidateSession"
+	traceID, err := s.validateContext(ctx, place)
 	if err != nil {
 		return nil, err
 	}
 	if req.SessionID == "" {
-		s.logger.Error("ValidateSession: Required sessionID",
-			zap.String("traceID", traceID),
-			zap.Error(erro.ErrorRequiredSessionId),
-		)
+		s.kafkaProducer.NewSessionLog(kafka.LogLevelWarn, place, traceID, "Required sessionID")
 		return nil, status.Errorf(codes.InvalidArgument, "SessionID is required")
 	}
 	if _, err := uuid.Parse(req.SessionID); err != nil {
-		s.logger.Error("ValidateSession: Error UUID-Parse sessionID",
-			zap.String("traceID", traceID),
-			zap.Error(err),
-		)
+		s.kafkaProducer.NewSessionLog(kafka.LogLevelWarn, place, traceID, "UUID-Parse sessionID error")
 		return nil, status.Errorf(codes.InvalidArgument, "Invalid sessionID format: %v", err)
 	}
 	response := s.repo.GetSession(ctx, req.SessionID)
@@ -103,22 +90,17 @@ func (s *SessionService) ValidateSession(ctx context.Context, req *pb.ValidateSe
 	}, nil
 }
 func (s *SessionService) DeleteSession(ctx context.Context, req *pb.DeleteSessionRequest) (*pb.DeleteSessionResponse, error) {
-	traceID, err := validateContext(ctx, s.logger, "DeleteSession")
+	var place = "DeleteSession"
+	traceID, err := s.validateContext(ctx, place)
 	if err != nil {
 		return nil, err
 	}
 	if req.SessionID == "" {
-		s.logger.Error("DeleteSession: Required sessionID",
-			zap.String("traceID", traceID),
-			zap.Error(erro.ErrorRequiredSessionId),
-		)
+		s.kafkaProducer.NewSessionLog(kafka.LogLevelWarn, place, traceID, "Required sessionID")
 		return nil, status.Errorf(codes.InvalidArgument, "SessionID is required")
 	}
 	if _, err := uuid.Parse(req.SessionID); err != nil {
-		s.logger.Error("DeleteSession: Error UUID-Parse sessionID",
-			zap.String("traceID", traceID),
-			zap.Error(err),
-		)
+		s.kafkaProducer.NewSessionLog(kafka.LogLevelWarn, place, traceID, "UUID-Parse sessionID error")
 		return nil, status.Errorf(codes.InvalidArgument, "Invalid sessionID format: %v", err)
 	}
 	response := s.repo.DeleteSession(ctx, req.SessionID)
