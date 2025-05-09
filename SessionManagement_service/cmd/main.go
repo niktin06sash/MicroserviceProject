df7da63d729cf857487e7c6310287a14ea88a1fc
nikitin_sas <nikitinsasha323@gmail.com>
2025-05-09 14:37:35 +0300

    implement kafka

--- SessionManagement_service/cmd/main.go
+++ SessionManagement_service/cmd/main.go
@@ -11,32 +11,23 @@ import (
 	"time"
 
 	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/configs"
-	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/logger"
+	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/kafka"
 	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/repository"
 	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/server"
 	"github.com/niktin06sash/MicroserviceProject/SessionManagement_service/internal/service"
-	"go.uber.org/zap"
 )
 
 func main() {
-	logger := logger.NewSessionLogger()
 	config := configs.LoadConfig()
-	redis, err := repository.NewRedisConnection(config.Redis, logger)
+	kafkaProducer := kafka.NewKafkaProducer(config.Kafka)
+	redis, err := repository.NewRedisConnection(config.Redis)
 	if err != nil {
-		logger.Fatal("SessionManagement: Failed to connect to database", zap.Error(err))
+		log.Fatalf("[ERROR] [Session-Service] Failed to connect to database: %v", err)
 		return
 	}
-	/*brokersString := config.Kafka.BootstrapServers
-	brokers := strings.Split(brokersString, ",")
-	kafkaProducer, err := kafka.NewKafkaProducer(brokers)
-	if err != nil {
-		log.Fatalf("Failed to create Kafka producer: %v", err)
-		return
-	}
-	defer kafkaProducer.Close()*/
-	repository := repository.NewRepository(redis, logger)
-	service := service.NewSessionAPI(repository, logger)
-	srv := server.NewGrpcServer(service, logger)
+	repository := repository.NewRepository(redis)
+	service := service.NewSessionAPI(repository)
+	srv := server.NewGrpcServer(service)
 	serverError := make(chan error, 1)
 	go func() {
 		if err := srv.Run(config.Server.Port); err != nil {
@@ -49,20 +40,21 @@ func main() {
 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
 	select {
 	case sig := <-quit:
-		logger.Info("SessionManagement: Service shutting down with signal", zap.String("signal", sig.String()))
+		log.Printf("[INFO] [Session-Service] Service shutting down with signal: %v", sig)
 	case err := <-serverError:
-		logger.Fatal("SessionManagement: Service startup failed", zap.Error(err))
+		log.Fatalf("[ERROR] [Session-Service] Service startup failed: %v", err)
 	}
 	shutdownTimeout := 5 * time.Second
 	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
 	defer cancel()
-	logger.Info("SessionManagement: Service is shutting down...")
+	log.Println("[INFO] [Session-Service] Service is shutting down...")
 	if err := srv.Shutdown(ctx); err != nil {
-		logger.Error("SessionManagement: Server shutdown error", zap.Error(err))
+		log.Printf("[ERROR] [Session-Service] Server shutdown error: %v", err)
 	}
-	logger.Info("SessionManagement: Service has shut down successfully")
+	log.Println("[INFO] [Session-Service] Service has shutted down successfully")
 	defer func() {
 		redis.Close()
+		kafkaProducer.Close()
 		buf := make([]byte, 10<<20)
 		n := runtime.Stack(buf, true)
 		log.Printf("Active goroutines:\n%s", buf[:n])
