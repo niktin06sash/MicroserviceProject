29909eab57c1a548a11ef0506be573af01c4a5bd
nikitin_sas <nikitinsasha323@gmail.com>
2025-05-10 17:48:10 +0300

    update main.go

--- UserManagement_service/cmd/main.go
+++ UserManagement_service/cmd/main.go
@@ -7,13 +7,13 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
-	"strings"
 	"syscall"
 	"time"
 
 	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/client"
 	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/configs"
 	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/handlers"
+	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/handlers/middleware"
 	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/kafka"
 	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/repository"
 	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/server"
@@ -24,21 +24,14 @@ func main() {
 	config := configs.LoadConfig()
 	db, err := repository.NewDatabaseConnection(config.Database)
 	if err != nil {
-		log.Fatalf("[ERROR] [UserManagement] Failed to connect to database: %v", err)
 		return
 	}
-	brokersString := config.Kafka.BootstrapServers
-	brokers := strings.Split(brokersString, ",")
-	kafkaProducer, err := kafka.NewKafkaProducer(brokers)
-	if err != nil {
-		log.Fatalf("[ERROR] [UserManagement] Failed to create Kafka producer: %v", err)
-		return
-	}
-	defer kafkaProducer.Close()
+	kafkaProducer := kafka.NewKafkaProducer(config.Kafka)
 	repositories := repository.NewRepository(db)
 	grpcclient := client.NewGrpcClient(config.SessionService)
 	service := service.NewService(repositories, kafkaProducer, grpcclient)
-	handlers := handlers.NewHandler(service)
+	middleware := middleware.NewMiddleware(kafkaProducer)
+	handlers := handlers.NewHandler(service, middleware, kafkaProducer)
 	srv := &server.Server{}
 	serverError := make(chan error, 1)
 	go func() {
@@ -54,7 +47,8 @@ func main() {
 	case sig := <-quit:
 		log.Printf("[INFO] [UserManagement] Service shutting down with signal: %v", sig)
 	case err := <-serverError:
-		log.Fatalf("[ERROR] [UserManagement] Service startup failed: %v", err)
+		log.Printf("[ERROR] [UserManagement] Service startup failed: %v", err)
+		return
 	}
 	shutdownTimeout := 5 * time.Second
 	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
@@ -62,9 +56,11 @@ func main() {
 	log.Println("[INFO] [UserManagement] Service is shutting down...")
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Printf("[ERROR] [UserManagement] Server shutdown error: %v", err)
+		return
 	}
 	log.Println("[INFO] [UserManagement] Service has shutted down successfully")
 	defer func() {
+		kafkaProducer.Close()
 		db.Close()
 		grpcclient.Close()
 		buf := make([]byte, 10<<20)
