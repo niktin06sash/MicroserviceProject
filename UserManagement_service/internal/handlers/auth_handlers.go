8b357fbc88eaf8e7061d9cecafac0bd11d642939
nikitin_sas <nikitinsasha323@gmail.com>
2025-05-10 18:46:26 +0300

    duplicate logic has been removed and c kafka logging has been added

--- UserManagement_service/internal/handlers/auth_handlers.go
+++ UserManagement_service/internal/handlers/auth_handlers.go
@@ -1,130 +1,74 @@
 package handlers
 
 import (
-	"encoding/json"
-	"io"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/erro"
 	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/handlers/response"
+	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/kafka"
 	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/model"
 
 	"github.com/google/uuid"
 )
 
 func (h *Handler) Registration(w http.ResponseWriter, r *http.Request) {
+	var place = "Registration"
 	defer r.Body.Close()
 	maparesponse := make(map[string]string)
 	traceID := r.Context().Value("traceID").(string)
-	if r.Method != http.MethodPost {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] Registration: Invalid request method(expected Post but it was sent %v)", traceID, r.Method)
-		maparesponse["ClientError"] = erro.ErrorNotPost.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "Registration")
-		return
-	}
-	datafromperson, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] Registration: ReadAll Error: %v", traceID, err)
-		maparesponse["ClientError"] = erro.ErrorReadAll.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "Registration")
+	if !checkMethod(r, w, http.MethodPost, traceID, maparesponse, place, h.KafkaProducer) {
 		return
 	}
 	var newperk model.Person
-	err = json.Unmarshal(datafromperson, &newperk)
-	if err != nil {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] Registration: Unmarshal Error: %v", traceID, err)
-		maparesponse["ClientError"] = erro.ErrorUnmarshal.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "Registration")
+	if !getAllData(r, w, traceID, maparesponse, place, &newperk, h.KafkaProducer) {
 		return
 	}
 	regresponse := h.Services.RegistrateAndLogin(r.Context(), &newperk)
-	if !regresponse.Success {
-		stringMap := response.ConvertErrorsToString(regresponse.Errors)
-		switch regresponse.Type {
-		case erro.ClientErrorType:
-			response.SendResponse(r.Context(), w, false, nil, stringMap, http.StatusBadRequest, traceID, "Registration")
-		case erro.ServerErrorType:
-			response.SendResponse(r.Context(), w, false, nil, stringMap, http.StatusInternalServerError, traceID, "Registration")
-		default:
-			log.Printf("[ERROR] [UserManagement] [TraceID: %s] Registration: Unknown error type", traceID)
-			maparesponse["InternalServerError"] = erro.ErrorInternalServer.Error()
-			response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusInternalServerError, traceID, "Registration")
-		}
+	if !serviceResponse(regresponse, r, w, traceID, place) {
 		return
 	}
-
 	respdata := map[string]any{"UserID": regresponse.UserId}
 	response.AddSessionCookie(w, regresponse.SessionId, regresponse.ExpireSession)
-	response.SendResponse(r.Context(), w, true, respdata, nil, http.StatusOK, traceID, "Registration")
-	log.Printf("[INFO] [UserManagement] [TraceID: %s] Registration: Person with id: %v has successfully registered", traceID, regresponse.UserId)
+	response.SendResponse(r.Context(), w, true, respdata, nil, http.StatusOK, traceID, place)
+	msg := fmt.Sprintf("Person with id %v has successfully registered", regresponse.UserId)
+	h.KafkaProducer.NewUserLog(kafka.LogLevelInfo, place, traceID, msg)
 }
 
 func (h *Handler) Authentication(w http.ResponseWriter, r *http.Request) {
+	var place = "Authentication"
 	defer r.Body.Close()
 	maparesponse := make(map[string]string)
 	traceID := r.Context().Value("traceID").(string)
-	if r.Method != http.MethodPost {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] Authentication: Invalid request method(expected Post but it was sent %v)", traceID, r.Method)
-		maparesponse["ClientError"] = erro.ErrorNotPost.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "Authentication")
-		return
-	}
-	datafromperson, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] Authentication: ReadAll Error: %v", traceID, err)
-		maparesponse["ClientError"] = erro.ErrorReadAll.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "Authentication")
+	if !checkMethod(r, w, http.MethodPost, traceID, maparesponse, place, h.KafkaProducer) {
 		return
 	}
 	var newperk model.Person
-	err = json.Unmarshal(datafromperson, &newperk)
-	if err != nil {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] Authentication: Unmarshal Error: %v", traceID, err)
-		maparesponse["ClientError"] = erro.ErrorUnmarshal.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "Authentication")
+	if !getAllData(r, w, traceID, maparesponse, place, &newperk, h.KafkaProducer) {
 		return
 	}
 	auresponse := h.Services.AuthenticateAndLogin(r.Context(), &newperk)
-	if !auresponse.Success {
-		stringMap := response.ConvertErrorsToString(auresponse.Errors)
-		switch auresponse.Type {
-		case erro.ClientErrorType:
-			response.SendResponse(r.Context(), w, false, nil, stringMap, http.StatusBadRequest, traceID, "Authentication")
-		case erro.ServerErrorType:
-			response.SendResponse(r.Context(), w, false, nil, stringMap, http.StatusInternalServerError, traceID, "Authentication")
-		default:
-			log.Printf("[ERROR] [UserManagement] [TraceID: %s] Authentication: Unknown error type", traceID)
-			maparesponse["InternalServerError"] = erro.ErrorInternalServer.Error()
-			response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusInternalServerError, traceID, "Authentication")
-		}
+	if !serviceResponse(auresponse, r, w, traceID, place) {
 		return
 	}
-
 	respdata := map[string]any{"UserID": auresponse.UserId}
 	response.AddSessionCookie(w, auresponse.SessionId, auresponse.ExpireSession)
-	response.SendResponse(r.Context(), w, true, respdata, nil, http.StatusOK, traceID, "Authentication")
-	log.Printf("[INFO] [UserManagement] [TraceID: %s] Authentication: Person with id: %v has successfully authenticated", traceID, auresponse.UserId)
+	response.SendResponse(r.Context(), w, true, respdata, nil, http.StatusOK, traceID, place)
+	msg := fmt.Sprintf("Person with id %v has successfully registered", auresponse.UserId)
+	h.KafkaProducer.NewUserLog(kafka.LogLevelInfo, place, traceID, msg)
 }
 
 func (h *Handler) DeleteAccount(w http.ResponseWriter, r *http.Request) {
+	var place = "DeleteAccount"
 	defer r.Body.Close()
 	maparesponse := make(map[string]string)
 	traceID := r.Context().Value("traceID").(string)
-	sessionID, ok := r.Context().Value("sessionID").(string)
-	if !ok {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] DeleteAccount: Session ID not found in context", traceID)
-		maparesponse["InternalServerError"] = erro.ErrorMissingSessionID.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusInternalServerError, traceID, "DeleteAccount")
+	flag, sessionID, userIDStr := getUserIdAndSession(r, w, traceID, maparesponse, place, h.KafkaProducer)
+	if !flag {
 		return
 	}
-	userIDStr, ok := r.Context().Value("userID").(string)
-	if !ok {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] DeleteAccount: User ID not found in context", traceID)
-		maparesponse["InternalServerError"] = erro.ErrorMissingUserID.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusInternalServerError, traceID, "DeleteAccount")
-		return
-	}
+	//сделать перевод в uuid в бизнес-логике - добавить в другой слой
 	userID, err := uuid.Parse(userIDStr)
 	if err != nil {
 		log.Printf("[ERROR] [UserManagement] [TraceID: %s] DeleteAccount: Invalid User ID format: %v", traceID, err)
@@ -132,100 +76,48 @@ func (h *Handler) DeleteAccount(w http.ResponseWriter, r *http.Request) {
 		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusInternalServerError, traceID, "DeleteAccount")
 		return
 	}
-	if r.Method != http.MethodDelete {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] DeleteAccount: Invalid request method(expected Delete but it was sent %v)", traceID, r.Method)
-		maparesponse["ClientError"] = erro.ErrorNotDelete.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "DeleteAccount")
+	if !checkMethod(r, w, http.MethodDelete, traceID, maparesponse, place, h.KafkaProducer) {
 		return
 	}
-	passwordBytes, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] DeleteAccount: ReadAll Error: %v", traceID, err)
-		maparesponse["ClientError"] = erro.ErrorReadAll.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "DeleteAccount")
-		return
-	}
-	var data map[string]string
-	if err := json.Unmarshal(passwordBytes, &data); err != nil {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] DeleteAccount: Invalid password format or empty password", traceID)
-		maparesponse["ClientError"] = erro.ErrorUnmarshal.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "DeleteAccount")
+	data := make(map[string]string)
+	if !getAllData(r, w, traceID, maparesponse, place, data, h.KafkaProducer) {
 		return
 	}
 	password, ok := data["password"]
 	if !ok || password == "" {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] DeleteAccount: Password is missing or empty", traceID)
+		h.KafkaProducer.NewUserLog(kafka.LogLevelError, place, traceID, "Password is missing or empty")
 		maparesponse["ClientError"] = erro.ErrorUnmarshal.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "DeleteAccount")
+		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, place)
 		return
 	}
 	defer r.Body.Close()
 	delresponse := h.Services.DeleteAccount(r.Context(), sessionID, userID, string(password))
-	if !delresponse.Success {
-		stringMap := response.ConvertErrorsToString(delresponse.Errors)
-		switch delresponse.Type {
-		case erro.ClientErrorType:
-			response.SendResponse(r.Context(), w, false, nil, stringMap, http.StatusBadRequest, traceID, "DeleteAccount")
-		case erro.ServerErrorType:
-			response.SendResponse(r.Context(), w, false, nil, stringMap, http.StatusInternalServerError, traceID, "DeleteAccount")
-		default:
-			log.Printf("[ERROR] [UserManagement] [TraceID: %s] DeleteAccount: Unknown error type", traceID)
-			maparesponse["InternalServerError"] = erro.ErrorInternalServer.Error()
-			response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusInternalServerError, traceID, "DeleteAccount")
-		}
+	if !serviceResponse(delresponse, r, w, traceID, place) {
 		return
 	}
 	response.DeleteSessionCookie(w)
-	response.SendResponse(r.Context(), w, true, nil, nil, http.StatusOK, traceID, "DeleteAccount")
-	log.Printf("[INFO] [UserManagement] [TraceID: %s] DeleteAccount: Person with id: %v has successfully delete account with all data", traceID, userID)
+	response.SendResponse(r.Context(), w, true, nil, nil, http.StatusOK, traceID, place)
+	msg := fmt.Sprintf("Person with id %v has successfully registered", delresponse.UserId)
+	h.KafkaProducer.NewUserLog(kafka.LogLevelInfo, place, traceID, msg)
 }
 func (h *Handler) Logout(w http.ResponseWriter, r *http.Request) {
+	var place = "Logout"
 	defer r.Body.Close()
 	maparesponse := make(map[string]string)
 	traceID := r.Context().Value("traceID").(string)
-	sessionID, ok := r.Context().Value("sessionID").(string)
-	if !ok {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] Logout: Session ID not found in context", traceID)
-		maparesponse["InternalServerError"] = erro.ErrorMissingSessionID.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusInternalServerError, traceID, "Logout")
+	flag, sessionID, userIDStr := getUserIdAndSession(r, w, traceID, maparesponse, place, h.KafkaProducer)
+	if !flag {
 		return
 	}
-	userIDStr, ok := r.Context().Value("userID").(string)
-	if !ok {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] Logout: User ID not found in context", traceID)
-		maparesponse["InternalServerError"] = erro.ErrorMissingUserID.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusInternalServerError, traceID, "Logout")
-		return
-	}
-	userID, err := uuid.Parse(userIDStr)
-	if err != nil {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] Logout: Invalid User ID format: %v", traceID, err)
-		maparesponse["InternalServerError"] = erro.ErrorMissingUserID.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusInternalServerError, traceID, "Logout")
-		return
-	}
-	if r.Method != http.MethodDelete {
-		log.Printf("[ERROR] [UserManagement] [TraceID: %s] Logout: Invalid request method(expected Delete but it was sent %v)", traceID, r.Method)
-		maparesponse["ClientError"] = erro.ErrorNotDelete.Error()
-		response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusBadRequest, traceID, "Logout")
+	if !checkMethod(r, w, http.MethodDelete, traceID, maparesponse, place, h.KafkaProducer) {
 		return
 	}
 	logresponse := h.Services.Logout(r.Context(), sessionID)
-	if !logresponse.Success {
-		stringMap := response.ConvertErrorsToString(logresponse.Errors)
-		switch logresponse.Type {
-		case erro.ClientErrorType:
-			response.SendResponse(r.Context(), w, false, nil, stringMap, http.StatusBadRequest, traceID, "Logout")
-		case erro.ServerErrorType:
-			response.SendResponse(r.Context(), w, false, nil, stringMap, http.StatusInternalServerError, traceID, "Logout")
-		default:
-			log.Printf("[ERROR] [UserManagement] [TraceID: %s] Logout: Unknown error type", traceID)
-			maparesponse["InternalServerError"] = erro.ErrorInternalServer.Error()
-			response.SendResponse(r.Context(), w, false, nil, maparesponse, http.StatusInternalServerError, traceID, "Logout")
-		}
+	if !serviceResponse(logresponse, r, w, traceID, place) {
 		return
 	}
 	response.DeleteSessionCookie(w)
 	response.SendResponse(r.Context(), w, true, nil, nil, http.StatusOK, traceID, "Logout")
-	log.Printf("[INFO] [UserManagement] [TraceID: %s] Logout: Person with id: %v has successfully logged out of the account", traceID, userID)
+	msg := fmt.Sprintf("Person with id %v has successfully logout", userIDStr)
+	h.KafkaProducer.NewUserLog(kafka.LogLevelInfo, place, traceID, msg)
 }
---
+++ UserManagement_service/internal/handlers/utils.go
@@ -0,0 +1,73 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/erro"
+	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/handlers/response"
+	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/kafka"
+	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/service"
+)
+
+func checkMethod(r *http.Request, w http.ResponseWriter, expectedMethod string, traceID string, mapa map[string]string, place string, kafkaproducer kafka.KafkaProducerService) bool {
+	if r.Method != expectedMethod {
+		fmterr := fmt.Sprintf("Invalid request method(expected %s but it was sent %v)", expectedMethod, r.Method)
+		kafkaproducer.NewUserLog(kafka.LogLevelError, place, traceID, fmterr)
+		mapa["ClientError"] = "Invalid request method"
+		response.SendResponse(r.Context(), w, false, nil, mapa, http.StatusBadRequest, traceID, place)
+		return false
+	}
+	return true
+}
+func getAllData[T any](r *http.Request, w http.ResponseWriter, traceID string, mapa map[string]string, place string, parser T, kafkaproducer kafka.KafkaProducerService) bool {
+	datafromperson, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmterr := fmt.Sprintf("ReadAll Error: %v", err)
+		kafkaproducer.NewUserLog(kafka.LogLevelError, place, traceID, fmterr)
+		mapa["ClientError"] = erro.ErrorReadAll.Error()
+		response.SendResponse(r.Context(), w, false, nil, mapa, http.StatusBadRequest, traceID, place)
+		return false
+	}
+	err = json.Unmarshal(datafromperson, parser)
+	if err != nil {
+		fmterr := fmt.Sprintf("Unmarshal Error: %v", err)
+		kafkaproducer.NewUserLog(kafka.LogLevelError, place, traceID, fmterr)
+		mapa["ClientError"] = erro.ErrorUnmarshal.Error()
+		response.SendResponse(r.Context(), w, false, nil, mapa, http.StatusBadRequest, traceID, place)
+		return false
+	}
+	return true
+}
+func getUserIdAndSession(r *http.Request, w http.ResponseWriter, traceID string, mapa map[string]string, place string, kafkaproducer kafka.KafkaProducerService) (bool, string, string) {
+	sessionID, ok := r.Context().Value("sessionID").(string)
+	if !ok {
+		kafkaproducer.NewUserLog(kafka.LogLevelError, place, traceID, "Session ID not found in context")
+		mapa["InternalServerError"] = erro.ErrorMissingSessionID.Error()
+		response.SendResponse(r.Context(), w, false, nil, mapa, http.StatusInternalServerError, traceID, place)
+		return false, "", ""
+	}
+	userID, ok := r.Context().Value("userID").(string)
+	if !ok {
+		kafkaproducer.NewUserLog(kafka.LogLevelError, place, traceID, "User ID not found in context")
+		mapa["InternalServerError"] = erro.ErrorMissingUserID.Error()
+		response.SendResponse(r.Context(), w, false, nil, mapa, http.StatusInternalServerError, traceID, place)
+		return false, "", ""
+	}
+	return true, sessionID, userID
+}
+func serviceResponse(resp *service.ServiceResponse, r *http.Request, w http.ResponseWriter, traceID string, place string) bool {
+	if !resp.Success {
+		stringMap := response.ConvertErrorsToString(resp.Errors)
+		switch resp.Type {
+		case erro.ClientErrorType:
+			response.SendResponse(r.Context(), w, false, nil, stringMap, http.StatusBadRequest, traceID, place)
+		case erro.ServerErrorType:
+			response.SendResponse(r.Context(), w, false, nil, stringMap, http.StatusInternalServerError, traceID, place)
+		}
+		return false
+	}
+	return true
+}
