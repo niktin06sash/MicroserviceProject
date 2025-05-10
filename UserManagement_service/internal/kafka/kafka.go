30821eef05e8d9090087b651a1a4433fe89e5195
nikitin_sas <nikitinsasha323@gmail.com>
2025-05-10 17:23:21 +0300

    implement kafka-producer

--- UserManagement_service/internal/kafka/kafka.go
+++ UserManagement_service/internal/kafka/kafka.go
@@ -2,54 +2,151 @@ package kafka
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"log"
+	"strings"
+	"sync"
 	"time"
 
+	"github.com/niktin06sash/MicroserviceProject/UserManagement_service/internal/configs"
 	"github.com/segmentio/kafka-go"
 )
 
-type KafkaProducer interface {
-	SendMessage(topic string, key string, value []byte) error
-	Close() error
+const (
+	LogLevelInfo  = "INFO"
+	LogLevelWarn  = "WARN"
+	LogLevelError = "ERROR"
+)
+
+type UserLog struct {
+	Level     string `json:"-"`
+	Service   string `json:"service"`
+	Place     string `json:"place"`
+	TraceID   string `json:"trace_id"`
+	Timestamp string `json:"timestamp"`
+	Message   string `json:"message"`
+}
+type KafkaProducer struct {
+	writer  *kafka.Writer
+	logchan chan UserLog
+	wg      *sync.WaitGroup
+	context context.Context
+	cancel  context.CancelFunc
+}
+type KafkaProducerService interface {
+	NewSessionLog(level, place, traceid, msg string)
 }
 
-type kafkaProducer struct {
-	writer *kafka.Writer
-}
-
-func NewKafkaProducer(brokers []string) (KafkaProducer, error) {
+func NewKafkaProducer(config configs.KafkaConfig) *KafkaProducer {
+	brokersString := config.BootstrapServers
+	brokers := strings.Split(brokersString, ",")
+	var acks kafka.RequiredAcks
+	switch config.Acks {
+	case "0":
+		acks = kafka.RequireNone
+	case "1":
+		acks = kafka.RequireOne
+	case "all":
+		acks = kafka.RequireAll
+	default:
+		acks = kafka.RequireAll
+	}
 	w := &kafka.Writer{
-		Addr:         kafka.TCP(brokers...), // Pass brokers as a slice of strings
-		Topic:        "",                    // Topic is specified in SendMessage
-		Balancer:     &kafka.LeastBytes{},   // Use LeastBytes balancer for even distribution
-		WriteTimeout: 10 * time.Second,      // Set a write timeout
+		Addr:            kafka.TCP(brokers...),
+		Topic:           "",
+		WriteTimeout:    10 * time.Second,
+		WriteBackoffMin: time.Duration(config.RetryBackoffMs) * time.Millisecond,
+		WriteBackoffMax: 5 * time.Second,
+		BatchSize:       config.BatchSize,
+		RequiredAcks:    acks,
 	}
-
-	return &kafkaProducer{writer: w}, nil
+	ctx, cancel := context.WithCancel(context.Background())
+	logs := make(chan UserLog, 1000)
+	producer := &KafkaProducer{
+		writer:  w,
+		logchan: logs,
+		wg:      &sync.WaitGroup{},
+		context: ctx,
+		cancel:  cancel,
+	}
+	startmsg := "Successful connect to Kafka-Producer"
+	startlog := UserLog{
+		Level:     LogLevelInfo,
+		Service:   "User-Service",
+		Timestamp: time.Now().Format(time.RFC3339),
+		Message:   startmsg,
+	}
+	for i := 1; i <= 3; i++ {
+		producer.wg.Add(1)
+		go producer.sendLogs(i)
+	}
+	producer.logchan <- startlog
+	log.Println("[INFO] [User-Service] [KafkaProducer] Successful connect to Kafka-Producer")
+	return producer
 }
-
-func (kp *kafkaProducer) SendMessage(topic string, key string, value []byte) error {
-	msg := kafka.Message{
-		Topic: topic,
-		Key:   []byte(key),
-		Value: value,
+func (kf *KafkaProducer) NewSessionLog(level, place, traceid, msg string) {
+	newlog := UserLog{
+		Level:     level,
+		Service:   "User-Service",
+		Place:     place,
+		TraceID:   traceid,
+		Timestamp: time.Now().Format(time.RFC3339),
+		Message:   msg,
 	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second) // Add context
-	defer cancel()
-
-	err := kp.writer.WriteMessages(ctx, msg) // Use WriteMessages with context
-	if err != nil {
-		return fmt.Errorf("failed to write messages: %w", err)
+	select {
+	case kf.logchan <- newlog:
+	default:
+		log.Printf("[WARN] [User-Service] [KafkaProducer] Log channel is full, dropping log: %+v", newlog)
 	}
-
-	return nil
 }
-
-func (kp *kafkaProducer) Close() error {
-	err := kp.writer.Close()
-	if err != nil {
-		return fmt.Errorf("failed to close writer: %w", err)
+func (kf *KafkaProducer) Close() {
+	close(kf.logchan)
+	kf.cancel()
+	kf.wg.Wait()
+	kf.writer.Close()
+	log.Println("[INFO] [User-Service] [KafkaProducer] Successful close Kafka-Producer")
+}
+func (kf *KafkaProducer) sendLogs(num int) {
+	defer kf.wg.Done()
+	for {
+		select {
+		case <-kf.context.Done():
+			return
+		case logg, ok := <-kf.logchan:
+			if !ok {
+				log.Printf("[INFO] [User-Service] [KafkaProducer] [Worker: %v] Log channel closed, stopping worker", num)
+				return
+			}
+			ctx, cancel := context.WithTimeout(kf.context, 5*time.Second)
+			defer cancel()
+			topic := "user-" + strings.ToLower(logg.Level) + "-log-topic"
+			data, err := json.Marshal(logg)
+			if err != nil {
+				log.Printf("[ERROR] [User-Service] [KafkaProducer] [Worker: %v] Failed to marshal log: %v", num, err)
+				continue
+			}
+		label:
+			for i := 0; i < 3; i++ {
+				select {
+				case <-ctx.Done():
+					log.Printf("[WARN] [User-Service] [KafkaProducer] [Worker: %v] Context canceled or expired, dropping log: %v", num, err)
+					continue
+				default:
+					err = kf.writer.WriteMessages(ctx, kafka.Message{
+						Topic: topic,
+						Key:   []byte(logg.TraceID),
+						Value: data,
+					})
+					if err == nil {
+						break label
+					}
+					log.Printf("[WARN] [User-Service] [KafkaProducer] [Worker: %v] Retry %d failed to send log: %v", num, i+1, err)
+					time.Sleep(1 * time.Second)
+				}
+			}
+			if err != nil {
+				log.Printf("[ERROR] [User-Service] [KafkaProducer] [Worker: %v] Failed to send log after all retries: %v, (%v)", num, err, logg)
+			}
+		}
 	}
-	return nil
 }
