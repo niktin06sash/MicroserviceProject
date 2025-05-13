3f21aa2185e87188cd36ae720a790fa209dacec9
nikitin_sas <nikitinsasha323@gmail.com>
2025-05-13 13:27:51 +0300

    implement metrics

--- UserManagement_service/go.mod
+++ UserManagement_service/go.mod
@@ -19,17 +19,24 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
-	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.22.0 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.12.0 // indirect
---
+++ UserManagement_service/internal/metrics/metrics.go
@@ -0,0 +1,71 @@
+package metrics
+
+import (
+	"log"
+	"runtime"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var UserTotalRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "user_service_requests_total",
+	Help: "Total number of requests to User-Service",
+}, []string{"path"})
+var UserRequestDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	Name:    "user_service_duration_seconds",
+	Help:    "Histogram for the request duration in seconds in User-Service",
+	Buckets: []float64{0.1, 0.5, 1, 2, 5},
+}, []string{"handler"})
+var UserErrorsTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "user_service_errors_total",
+	Help: "Total number of errors encountered by the User-Service",
+}, []string{"error_type"})
+var UserTotalSuccessfulRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "user_service_successful_requests_total",
+	Help: "Total number of successful requests to User-Service",
+}, []string{"handler"})
+var UserBackendRequestsTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "user_service_backend_requests_total",
+	Help: "Total number of requests forwarded to backend services",
+}, []string{"service"})
+var UserMemoryUsage = promauto.NewGauge(prometheus.GaugeOpts{
+	Name: "user_service_memory_usage_bytes",
+	Help: "Current memory usage in bytes",
+})
+var UserDBQueryDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	Name:    "user_service_db_query_duration_seconds",
+	Help:    "Histogram for the query duration in seconds to the database",
+	Buckets: []float64{0.01, 0.05, 0.1, 0.5, 1},
+}, []string{"query_type"})
+var UserDBQueriesTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "user_service_db_queries_total",
+	Help: "Total number of queries executed on the database",
+}, []string{"query_type"})
+var UserDBErrorsTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "user_service_db_errors_total",
+	Help: "Total number of errors encountered when interacting with the database",
+}, []string{"error_type"})
+var stop = make(chan struct{})
+
+func Start() {
+	go func() {
+		ticker := time.NewTicker(10 * time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				var memStats runtime.MemStats
+				runtime.ReadMemStats(&memStats)
+				UserMemoryUsage.Set(float64(memStats.Alloc))
+			case <-stop:
+				return
+			}
+		}
+	}()
+}
+func Stop() {
+	close(stop)
+	log.Println("[INFO] [User-Service] Successful close Metrics-Goroutine")
+}
