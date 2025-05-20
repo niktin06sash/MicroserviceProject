30ebb215d0541dcc045e84755e22fa6c3ec4f4e6
nikitin_sas <nikitinsasha323@gmail.com>
2025-05-20 11:45:10 +0300

    fix bugs between start locally and in docker

--- API_service/internal/configs/config.go
+++ API_service/internal/configs/config.go
@@ -1,7 +1,10 @@
 package configs
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -40,20 +43,55 @@ func LoadConfig() Config {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yml")
 	viper.AddConfigPath("internal/configs")
-
 	err := viper.ReadInConfig()
 	if err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			log.Printf("[ERROR] [API-Service] Config file not found; using defaults or environment variables")
+			log.Printf("[DEBUG] [API-Service] Config file not found; using defaults or environment variables")
 		} else {
-			log.Fatalf("[ERROR] [API-Service] Error reading config file: %s", err)
+			log.Fatalf("[DEBUG] [API-Service] Error reading config file: %s", err)
 		}
 	}
 	var config Config
 	err = viper.Unmarshal(&config)
 	if err != nil {
-		log.Fatalf("[ERROR] [API-Service] Unable to decode into struct, %v", err)
+		log.Fatalf("[DEBUG] [API-Service] Unable to decode into struct, %v", err)
 	}
-	log.Println("[INFO] [API-Service] Successful Load Config")
+	docker_flag := os.Getenv("DOCKER")
+	if docker_flag == "TRUE" {
+		LoadDockerConfig(&config)
+		log.Println("[DEBUG] [API-Service] Successful Load Config (docker)")
+		return config
+	}
+	log.Println("[DEBUG] [API-Service] Successful Load Config (localhost)")
 	return config
+}
+func LoadDockerConfig(config *Config) {
+	grpcaddr := os.Getenv("SESSION_SERVICE_GRPC_ADDRESS")
+	userservice := os.Getenv("USER_SERVICE_HOST")
+	kafka := os.Getenv("KAFKA_BOOTSTRAP_SERVERS")
+	config.SessionService.GrpcAddress = grpcaddr
+	config.Kafka.BootstrapServers = kafka
+	config.Routes = UpdateRoutes(config.Routes, userservice)
+}
+func UpdateRoutes(routes map[string]string, host string) map[string]string {
+	updatedRoutes := make(map[string]string)
+	for path, url := range routes {
+		updatedRoutes[path] = replaceHost(url, host)
+	}
+	return updatedRoutes
+}
+
+func replaceHost(url, newHost string) string {
+	parts := strings.SplitN(url, "://", 2)
+	if len(parts) != 2 {
+		return url
+	}
+	protocol := parts[0]
+	address := parts[1]
+	hostAndPath := strings.SplitN(address, "/", 2)
+	if len(hostAndPath) < 1 {
+		return url
+	}
+	newAddress := fmt.Sprintf("%s/%s", newHost, hostAndPath[1])
+	return fmt.Sprintf("%s://%s", protocol, newAddress)
 }
