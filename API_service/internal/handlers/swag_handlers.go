866f8ce2d1554495b5eb0e38890273f5b05e3f8c
nikitin_sas <nikitinsasha323@gmail.com>
2025-06-13 20:00:14 +0300

    implemented the first handler (load photo) in the api service

--- API_service/internal/handlers/swag_handlers.go
+++ API_service/internal/handlers/swag_handlers.go
@@ -1,8 +1,18 @@
 package handlers
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/niktin06sash/MicroserviceProject/API_service/internal/brokers/kafka"
+	"github.com/niktin06sash/MicroserviceProject/API_service/internal/erro"
+	"github.com/niktin06sash/MicroserviceProject/API_service/internal/handlers/response"
 	_ "github.com/niktin06sash/MicroserviceProject/API_service/internal/handlers/response"
+	"github.com/niktin06sash/MicroserviceProject/API_service/internal/metrics"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // @Summary Register a new user
@@ -98,28 +108,110 @@ func (h *Handler) MyProfile(c *gin.Context) {
 
 }
 
-// @Summary Find user by ID in path parameters
-// @Description Retrieves a user's profile data by sending a request with the user's ID as a path parameter.
+// @Summary Find userprofile by ID in path parameters
+// @Description Retrieves a user's profile data by sending a request with the userID as a path parameter.
 // @Tags User Management
 // @Produce json
-// @Param id path string true "User ID in UUID format"
+// @Param id path string true "userID in UUID format"
 // @Success 200 {object} response.HTTPResponse "User profile data successfully retrieved"
 // @Failure 400 {object} response.HTTPResponse "Invalid input data"
 // @Failure 401 {object} response.HTTPResponse "Unauthorized"
 // @Failure 429 {object} response.HTTPResponse "Too many requests"
 // @Failure 500 {object} response.HTTPResponse "Internal server error"
-// @Router /api/users/id/{id} [get]
-func (h *Handler) GetUserById(c *gin.Context) {
+// @Router /api/users/{id} [get]
+func (h *Handler) GetUserProfileById(c *gin.Context) {
 
 }
 
+// @Summary Find photo by ID's in path parameters
+// @Description Retrieves a user's photo by sending a request with the userID, photoID as a path parameter.
+// @Tags Photo Service
+// @Produce json
+// @Param id path string true "userID in UUID format"
+// @Param photo_id path string true "photoID in UUID format"
+// @Success 200 {object} response.HTTPResponse "User's photo successfully retrieved"
+// @Failure 400 {object} response.HTTPResponse "Invalid input data"
+// @Failure 401 {object} response.HTTPResponse "Unauthorized"
+// @Failure 429 {object} response.HTTPResponse "Too many requests"
+// @Failure 500 {object} response.HTTPResponse "Internal server error"
+// @Router /api/users/{id}/photos/{photo_id} [get]
 func (h *Handler) GetPhotoById(c *gin.Context) {
 }
 
+// @Summary Delete photo by ID's in path parameters
+// @Description Retrieves a own photo by sending a request with the photoID as a path parameter.
+// @Tags Photo Service
+// @Produce json
+// @Param photo_id path string true "photoID in UUID format"
+// @Success 200 {object} response.HTTPResponse "Photo successfully deleted"
+// @Failure 400 {object} response.HTTPResponse "Invalid input data"
+// @Failure 401 {object} response.HTTPResponse "Unauthorized"
+// @Failure 429 {object} response.HTTPResponse "Too many requests"
+// @Failure 500 {object} response.HTTPResponse "Internal server error"
+// @Router /api/me/photos/{photo_id} [delete]
 func (h *Handler) DeletePhoto(c *gin.Context) {
 }
 
+// @Summary Upload user photo
+// @Description Uploads a new photo for user
+// @Tags Photo Service
+// @Accept multipart/form-data
+// @Produce json
+// @Param photo formData file true "Image file (JPG/PNG)"
+// @Success 200 {object} response.HTTPResponse "Photo successfully uploaded"
+// @Failure 400 {object} response.HTTPResponse "Invalid input data"
+// @Failure 401 {object} response.HTTPResponse "Unauthorized"
+// @Failure 429 {object} response.HTTPResponse "Too many requests"
+// @Failure 500 {object} response.HTTPResponse "Internal server error"
+// @Router /api/me/photos [post]
 func (h *Handler) LoadPhoto(c *gin.Context) {
+	const place = LoadPhoto
+	traceID := c.MustGet("traceID").(string)
+	userid := c.MustGet("userID").(string)
+	file, _, err := c.Request.FormFile("photo")
+	if err != nil {
+		h.logproducer.NewAPILog(c.Request, kafka.LogLevelWarn, place, traceID, erro.RequiredFormPhoto)
+		response.BadResponse(c, http.StatusBadRequest, erro.RequiredFormPhoto, traceID, place, h.logproducer)
+		c.Abort()
+		metrics.APIErrorsTotal.WithLabelValues(erro.ClientErrorType).Inc()
+		return
+	}
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		h.logproducer.NewAPILog(c.Request, kafka.LogLevelWarn, place, traceID, fmt.Sprintf("Failed readAll: %v", err))
+		response.BadResponse(c, http.StatusBadRequest, erro.APIServiceUnavalaible, traceID, place, h.logproducer)
+		c.Abort()
+		metrics.APIErrorsTotal.WithLabelValues(erro.ServerErrorType).Inc()
+		return
+	}
+	protoresponse, err := h.photoclient.LoadPhoto(c.Request.Context(), userid, bytes)
+	if err == nil && protoresponse != nil && protoresponse.Status {
+		response.OkResponse(c, http.StatusOK, map[string]any{response.KeyMessage: protoresponse.Message, response.KeyPhotoID: protoresponse.PhotoId}, traceID, place, h.logproducer)
+		return
+	}
+	st, _ := status.FromError(err)
+	switch st.Code() {
+	case codes.Internal, codes.Canceled, codes.Unavailable:
+		response.BadResponse(c, http.StatusInternalServerError, erro.PhotoServiceUnavalaible, traceID, place, h.logproducer)
+		c.Abort()
+		return
+	default:
+		response.BadResponse(c, http.StatusBadRequest, st.Message(), traceID, place, h.logproducer)
+		c.Abort()
+		return
+	}
 }
+
+// @Summary Find own photo by ID's in path parameters
+// @Description Retrieves a own photo by sending a request with the photoID as a path parameter.
+// @Tags Photo Service
+// @Produce json
+// @Param photo_id path string true "photoID in UUID format"
+// @Success 200 {object} response.HTTPResponse "Own photo successfully retrieved"
+// @Failure 400 {object} response.HTTPResponse "Invalid input data"
+// @Failure 401 {object} response.HTTPResponse "Unauthorized"
+// @Failure 429 {object} response.HTTPResponse "Too many requests"
+// @Failure 500 {object} response.HTTPResponse "Internal server error"
+// @Router /api/me/photos/{photo_id} [get]
 func (h *Handler) GetMyPhotoById(c *gin.Context) {
 }
